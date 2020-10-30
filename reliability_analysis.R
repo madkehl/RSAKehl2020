7322939b807c239638e30d24fbf2d62193040366
Madeline Kehl <mad.kehl@gmail.com>
2020-10-30 14:06:53 -0500

    feat: reliability rescaling scripts

---
+++ reliability_analysis.R
@@ -0,0 +1,61 @@
+library(psych)
+
+library(dplyr)
+file1 <- read.csv('/Users/madke/downloads/MadelinesThesis_ForReliabilities.csv')
+
+
+file_df <- (file1) %>% mutate_all(scale)
+file_df <- data.frame(file_df)
+#CSIV
+PA <- file_df %>% dplyr::select('CSIV01','CSIV09','CSIV17','CSIV25','CSIV33','CSIV41','CSIV49','CSIV57')
+BC <- file_df %>% dplyr::select('CSIV04','CSIV12','CSIV20','CSIV28','CSIV36','CSIV44','CSIV52','CSIV60')
+DE <- file_df %>% dplyr::select('CSIV07','CSIV15','CSIV23','CSIV31','CSIV39','CSIV47','CSIV55','CSIV63')
+FG <- file_df %>% dplyr::select('CSIV02','CSIV10','CSIV18','CSIV26','CSIV34','CSIV42','CSIV50','CSIV58')
+HI <- file_df %>% dplyr::select('CSIV05','CSIV13','CSIV21','CSIV29','CSIV37','CSIV45','CSIV53','CSIV61')
+JK <- file_df %>% dplyr::select('CSIV08','CSIV16','CSIV24','CSIV32','CSIV40','CSIV48','CSIV56','CSIV64')
+LM <- file_df %>% dplyr::select('CSIV03','CSIV11','CSIV19','CSIV27','CSIV35','CSIV43','CSIV51','CSIV59')
+NO <- file_df %>% dplyr::select('CSIV06','CSIV14','CSIV22','CSIV30','CSIV38','CSIV46','CSIV54','CSIV62')
+
+
+#CSIE
+PA <- file_df %>% dplyr::select('CSIE04','CSIE12','CSIE20','CSIE28')
+BC <- file_df %>% dplyr::select('CSIE07','CSIE15','CSIE23','CSIE31')
+DE <- file_df %>% dplyr::select('CSIE02','CSIE10','CSIE18','CSIE26')
+FG <- file_df %>% dplyr::select('CSIE05','CSIE13','CSIE21','CSIE29')
+HI <- file_df %>% dplyr::select('CSIE08','CSIE16','CSIE24','CSIE32')
+JK <- file_df %>% dplyr::select('CSIE03','CSIE11','CSIE19','CSIE27')
+LM <- file_df %>% dplyr::select('CSIE06','CSIE14','CSIE22','CSIE30')
+NO <- file_df %>% dplyr::select('CSIE01','CSIE09','CSIE17','CSIE25')
+
+#IIPSC
+PA <- file_df %>% dplyr::select('IIP01','IIP09','IIP17','IIP25')
+BC <- file_df %>% dplyr::select('IIP02','IIP10','IIP18','IIP26')
+DE <- file_df %>% dplyr::select('IIP03','IIP11','IIP19','IIP27')
+FG <- file_df %>% dplyr::select('IIP04','IIP12','IIP20','IIP28')
+HI <- file_df %>% dplyr::select('IIP05','IIP13','IIP21','IIP29')
+JK <- file_df %>% dplyr::select('IIP06','IIP14','IIP22','IIP30')
+LM <- file_df %>% dplyr::select('IIP07','IIP15','IIP23','IIP31')
+NO <- file_df %>% dplyr::select('IIP08','IIP16','IIP24','IIP32')
+
+
+om_PA = omega(PA, nfactors = 1)
+om_BC = omega(BC, nfactors = 1)
+om_DE = omega(DE, nfactors = 1)
+om_FG = omega(FG, nfactors = 1)
+om_HI = omega(HI, nfactors = 1)
+om_JK = omega(JK, nfactors = 1)
+om_LM = omega(LM, nfactors = 1)
+om_NO = omega(NO, nfactors = 1)
+
+#warmth
+w_sigwrelsc = (om_LM$omega.tot*1*1) + (om_DE$omega.tot*-1*-1) + (om_JK$omega.tot*0.707*0.707) + (om_NO$omega.tot*0.707*0.707) + (om_FG$omega.tot*-0.707*-0.707)+ (om_BC$omega.tot*-0.707*-0.707)
+w_var_axis = (var(warm_iip, na.rm = T))
+
+w_rel_axis = 1 - ((4 - w_sigwrelsc) /w_var_axis)
+describe(file_df1$IIPSC_ELEV)
+describe(file_df1$IIPSC_LOV)
+describe(file_df1$IIPSC_DOM)
+
+
+
+
---
+++ rescale_not_std.R
@@ -0,0 +1,57 @@
+library(dplyr)
+library(psych)
+file1 <- read.csv('/Users/madke/documents/051220_undergraduatesample.csv')
+file1 <- read.csv('/Users/madke/downloads/IPCx3 items Kehl.csv')
+
+reCSIE <- function(x){
+
+	x <- x/(2.2)
+#	x <- scale(x)
+	x
+
+}
+
+reCSIV <- function(x){
+
+	
+#	x <- scale(x)
+	x
+}
+
+CSIE <- file1 %>% select(CSIE_PA, CSIE_BC, CSIE_DE, CSIE_FG, CSIE_HI, CSIE_JK, CSIE_LM, CSIE_NO)
+CSIV <- file1 %>% select(CSIV_PA, CSIV_BC, CSIV_DE, CSIV_FG, CSIV_HI, CSIV_JK, CSIV_LM, CSIV_NO)
+IIPSC <- file1 %>% select(IIPSC_PA, IIPSC_BC, IIPSC_DE, IIPSC_FG, IIPSC_HI,IIPSC_JK, IIPSC_LM, IIPSC_NO)
+IIPSC <- file1 %>% select(iipscpa, iipscbc, iipscde, iipscfg, iipschi,iipscjk,iipsclm, iipscno)
+colnames(IIPSC) <- c('IIPSC_PA', 'IIPSC_BC', 'IIPSC_DE', 'IIPSC_FG', 'IIPSC_HI','IIPSC_JK', 'IIPSC_LM', 'IIPSC_NO')
+
+CSIE <- CSIE %>% mutate_all(reCSIE)
+#CSIV <- CSIV %>% mutate_all(reCSIV)
+#for reliabilities
+IIPSC <- IIPSC%>% mutate_all(scale)
+
+#checks out on hopwood
+CSIV['CSIV_PA'] <- (CSIV$CSIV_PA-2.53)/.63
+CSIV['CSIV_BC'] <- (CSIV$CSIV_BC-1.38)/.71
+CSIV['CSIV_DE']<-(CSIV$CSIV_DE-1.10)/.70
+CSIV['CSIV_FG'] <-(CSIV$CSIV_FG-1.66)/.78
+CSIV['CSIV_HI'] <-(CSIV$CSIV_HI-1.77)/.75
+CSIV['CSIV_JK'] <-(CSIV$CSIV_JK-2.676)/.71
+CSIV['CSIV_LM'] <- (CSIV$CSIV_LM-2.83)/.69
+CSIV['CSIV_NO'] <-(CSIV$CSIV_NO-2.93)/.57
+
+
+
+
+dom_csie <- 0.25*((CSIE$CSIE_PA - CSIE$CSIE_HI) + (0.707*(CSIE$CSIE_BC +  CSIE$CSIE_NO -  CSIE$CSIE_FG -  CSIE$CSIE_JK)))
+warm_csie <- 0.25*((CSIE$CSIE_LM - CSIE$CSIE_DE) + (0.707*(CSIE$CSIE_JK + CSIE$CSIE_NO - CSIE$CSIE_FG - CSIE$CSIE_BC)))
+
+
+dom_csiv <- 0.25*((CSIV$CSIV_PA - CSIV$CSIV_HI) + (0.707*(CSIV$CSIV_BC + CSIV$CSIV_NO - CSIV$CSIV_FG - CSIV$CSIV_JK)))
+warm_csiv <- 0.25*((CSIV$CSIV_LM - CSIV$CSIV_DE) + (0.707*(CSIV$CSIV_JK + CSIV$CSIV_NO - CSIV$CSIV_FG - CSIV$CSIV_BC)))
+
+elev_iip <- rowSums(IIPSC)*0.125
+dom_iip <-0.25*((IIPSC$IIPSC_PA - IIPSC$IIPSC_HI) + (0.707*(IIPSC$IIPSC_BC + IIPSC$IIPSC_NO - IIPSC$IIPSC_FG - IIPSC$IIPSC_JK)))
+warm_iip <-0.25*((IIPSC$IIPSC_LM - IIPSC$IIPSC_DE) + (0.707*(IIPSC$IIPSC_JK + IIPSC$IIPSC_NO - IIPSC$IIPSC_FG - IIPSC$IIPSC_BC)))
+
+mean(warm2, na.rm = T)
+
